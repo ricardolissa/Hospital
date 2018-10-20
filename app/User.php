6045a4d16572053e47e042241d74eaec774a8aa5
Ricardo <trakatraka@gmail.com>
2018-10-20 19:21:56 -0300

    borre

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
     ];
     /**
      * The attributes that should be hidden for arrays.
-     * Prueba de inicio
+     * 
      * @var array
      */
     protected $hidden = [
