f40ca09860ef32b90cea346d66800c9118669b6a
Ricardo <trakatraka@gmail.com>
2018-11-23 17:32:21 -0300

    se modifico tablas obrasocial y se crearon tablas pivote

---
+++ database/migrations/2018_11_23_202441_create_medico_especialidad.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicoEspecialidad extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medico_especialidads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('medico_id')->unsigned();
+            $table->integer('especialidad_id')->unsigned();
+            
+            
+            $table->foreign('medico_id')->references('id')->on('medicos')->onDelete('cascade');
+            $table->foreign('especialidad_id')->references('id')->on('especialidads')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medico_especialidads');
+    }
+}
---
+++ resources/views/medicos/indexmed.blade.php
@@ -0,0 +1,1 @@
+<div>Index Medicos</div>
