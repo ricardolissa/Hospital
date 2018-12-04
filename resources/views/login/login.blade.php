1082912220528c9f19f6948f24acefc3460de739
Crisssv <cristian.villarreal1989@gmail.com>
2018-12-04 18:10:16 -0300

    no se porque

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
 <html lang="en">
- <div class="panel panel-default">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -21,22 +20,27 @@
   </head>
 
   <body class="text-center">
+   
+    <div class= "col-sm-4"></div>
+    <div class= "col-sm-4">
+    <br>  
     <form class="form-signin">
       <img class="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+      <br>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Usuario" required autofocus>
+      <br>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
       <div class="checkbox mb-3">
         <label>
-          <input type="checkbox" value="remember-me"> Remember me
+          <input type="checkbox" value="remember-me"> Recordarme
         </label>
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Ingresar</button>
       <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
     </form>
-  </body>
-</div>
+  </div>
+  <div class= "col-sm-4"></div>
+  </body> 
 </html>
 @endsection
