ef9b79aff2782d809ab4e48297075d2b3d147874
Ricardo <trakatraka@gmail.com>
2018-12-04 19:04:39 -0300

    layout mejorado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,79 +1,196 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+            <meta content="IE=edge" http-equiv="X-UA-Compatible">
+                <meta content="width=device-width, initial-scale=1" name="viewport">
+                    <!-- CSRF Token -->
+                    <meta content="{{ csrf_token() }}" name="csrf-token">
+                        <title>
+                            {{ config('app.name', 'Laravel') }}
+                        </title>
+                        <!-- Scripts -->
+                        <script defer="" src="{{ asset('js/app.js') }}">
+                        </script>
+                        <!-- Fonts -->
+                        <link href="https://fonts.gstatic.com" rel="dns-prefetch">
+                            <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+                                <!-- Styles -->
+                                <link crossorigin="anonymous" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" rel="stylesheet">
+                                    {{--
+                                    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+                                        --}}
+                                        <style>
+                                            body {
+            padding-top: 65px;
+            padding-bottom: 20px;
+        }
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        /* Set padding to keep content from hitting the edges */
+        .body-content {
+            padding-left: 15px;
+            padding-right: 15px;
+        }
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        /* Override the default bootstrap behavior where horizontal description lists 
+           will truncate terms that are too long to fit in the left column.
+           Also, add a 8pm to the bottom margin
+        */
+        .dl-horizontal dt {
+            white-space: normal;
+            margin-bottom: 8px;
+        }
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+        /* Set width on the form input elements since they're 100% wide by default */
+        input,
+        select,
+        textarea,
+        .uploaded-file-group,
+        .input-width-input {
+            max-width: 380px;
+        }
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+        .input-delete-container {
+            width: 46px !important;
+        }
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        /* Vertically align the table cells inside body-panel */
+        .panel-body .table > tr > td
+        {
+            vertical-align: middle;
+        }
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+        .panel-body-with-table
+        {
+            padding: 0;
+        }
 
-                    </ul>
+        .mt-5 {
+            margin-top: 5px !important;
+        }
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+        .mb-5 {
+            margin-bottom: 5px !important;
+        }
+                                        </style>
+                                    </link>
+                                </link>
+                            </link>
+                        </link>
+                    </meta>
+                </meta>
+            </meta>
+        </meta>
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-inverse navbar-fixed-top">
+                <div class="container">
+                    <button aria-controls="navbar" aria-expanded="false" class="navbar-toggle collapsed" data-target="#navbar" data-toggle="collapse" type="button">
+                    </button>
+                    <a class="navbar-brand" href="{!! url('/') !!}">
+                        <img src="images/35x35logo.png"/>
+                    </a>
+                    <div class="collapse navbar-collapse" id="navbar">
+                        <ul class="nav navbar-nav">
+                            <li class="active">
+                                <a href="{!! url('/') !!}">
+                                    Home
                                 </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
                             </li>
+                        </ul>
+                        <ul class="nav navbar-nav">
+                            <li class="active">
+                                <a href="{!! route('regpacientes.regpaciente.index') !!}">
+                                    Buscar Paciente
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="nav navbar-nav">
+                            <li class="active">
+                                <a href="{!! route('regpacientes.regpaciente.cpaciente') !!}">
+                                    Nuevo Paciente
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="nav navbar-nav">
+                            <li class="active">
+                                <a href="{!! url('/') !!}">
+                                    Lista de espera
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">
+                                {{ __('Login') }}
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">
+                                {{ __('Register') }}
+                            </a>
+                        </li>
+                        @else
+                        <li class="nav-item dropdown">
+                            <a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="navbarDropdown" role="button" v-pre="">
+                                {{ Auth::user()->name }}
+                                <span class="caret">
+                                </span>
+                            </a>
+                            
+
+
+
+
+
+
+
+                            <div aria-labelledby="navbarDropdown" class="nav navbar-nav">
+                                <a class="nav navbar-nav" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form action="{{ route('logout') }}" id="logout-form" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                            <!--/.nav-collapse -->
+                        </li>
                         @endguest
-                    </ul>
+                    </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+            <div class="container body-content">
+                @yield('content')
+            </div>
+            
+            <script crossorigin="anonymous" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" src="https://code.jquery.com/jquery-1.12.4.min.js">
+            </script>
+            
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js">
+            </script>
+            {{--
+            <script src="{{ asset('js/app.js') }}">
+            </script>
+            --}}
+            <script type="text/javascript">
+                $(function(){
+
+             // sends the uploaded file file to the fielselect event
+            $(document).on('change', ':file', function() {
+                var input = $(this);
+                var label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+
+                input.trigger('fileselect', [label]);
+            });
+            </script>
+        
+
+    </body>
 </html>
